da084cb684e55edfd285f2a023b3063a1cd47bec
Scherbakova <nastyasch0806@gmail.com>
2022-09-30 00:45:08 +0300

    коммит с выполнением домашнего задания 1 и 2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-      /*  Person Maksim = new Person("Максим", "Минск", 1987, "бренд-менеджера");
+       Person Maksim = new Person("Максим", "Минск", 1987, "бренд-менеджера");
         Maksim.print();
         Person Anya = new Person("Аня", "Москва", 1993, "методиста образовательных программ");
         Anya.print();
         Person Katya = new Person("Катя", "Калининград", 1994, "продакт-менеджера");
         Katya.print();
         Person Artem = new Person("Артем", "Москва", 1995, "директора по развитию бизнеса");
-        Artem.print();*/
+        Artem.print();
 
         Car car1 = new Car("Lada", "Grande", "желтый", "России", 1.7, 2015);
         Car car2 = new Car("Audi", "A8 50 L TDI quattro", "черный", "Германии", 3.8, 2020);
