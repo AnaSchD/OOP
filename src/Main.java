7f39139daf411855e6717ca4c5f2281edb3f5585
Scherbakova <nastyasch0806@gmail.com>
2022-09-30 00:20:01 +0300

    коммит со вторым заданием

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        Person Maksim = new Person("Максим", "Минск", 1987);
+        Person Maksim = new Person("Максим", "Минск", 1987, "бренд-менеджера");
         Maksim.print();
-        Person Anya = new Person("Аня", "Москва", 1993);
+        Person Anya = new Person("Аня", "Москва", 1993, "методиста образовательных программ");
         Anya.print();
-        Person Katya = new Person("Катя", "Калининград", 1994);
+        Person Katya = new Person("Катя", "Калининград", 1994, "продакт-менеджера");
         Katya.print();
-        Person Artem = new Person("Артем", "Москва", 1995);
+        Person Artem = new Person("Артем", "Москва", 1995, "директора по развитию бизнеса");
         Artem.print();
 
 
--- src/Person.java
+++ src/Person.java
@@ -4,14 +4,18 @@ public class Person {
     String town;
     int yearOfBirth;
 
-    Person (String name, String town, int yearOfBirth) {
+    String job;
+
+    Person (String name, String town, int yearOfBirth, String job) {
         this.name = name;
         this.town = town;
         this.yearOfBirth = yearOfBirth;
+        this.job = job;
     }
 
     void print() {
-        System.out.println("Привет! Меня зовут " + name + "." + " Я из города " + town + "." + " Я родился в " + yearOfBirth + " году." + " Будем знакомы!");
+        System.out.println("Привет! Меня зовут " + name + "." + " Я из города " + town + "." +
+                " Я родился в " + yearOfBirth + " году." + " Я работаю на должности " + job + "." + " Будем знакомы!");
     }
 
 
