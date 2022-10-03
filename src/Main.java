e447ac5c18354826441a1507bfe29e28cb04a9bf
Scherbakova <nastyasch0806@gmail.com>
2022-10-03 23:14:45 +0300

    коммит с выполнением домашнего задания 1, 2 и 3

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-       Person Maksim = new Person("Максим", "Минск", 1987, "бренд-менеджера");
+        Person Maksim = new Person("Максим", null, 1987, "бренд-менеджера");
         Maksim.print();
         Person Anya = new Person("Аня", "Москва", 1993, "методиста образовательных программ");
         Anya.print();
-        Person Katya = new Person("Катя", "Калининград", 1994, "продакт-менеджера");
+        Person Katya = new Person(null, "Калининград", -1, "продакт-менеджера");
         Katya.print();
-        Person Artem = new Person("Артем", "Москва", 1995, "директора по развитию бизнеса");
+        Person Artem = new Person("Артем", "Москва", 1995, null);
         Artem.print();
 
-        Car car1 = new Car("Lada", "Grande", "желтый", "России", 1.7, 2015);
+      Car car1 = new Car("Lada", "Grande", null, "России", 1.7, 2015);
         Car car2 = new Car("Audi", "A8 50 L TDI quattro", "черный", "Германии", 3.8, 2020);
-        Car car3 = new Car("BMW", "Z8", "черный", "Германии", 3.0, 2021);
+        Car car3 = new Car("BMW", "Z8", "черный", null, 3.0, 2021);
         Car car4 = new Car("Kia", "Sportage 4 поколение", "красный", "Южной Корее", 2.4, 2018);
         Car car5 = new Car("Hyundai", "Avante", "оранжевый", "Южной Корее", 1.6, 2016);
         car1.print();
